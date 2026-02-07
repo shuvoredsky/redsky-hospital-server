bf2d5419eb1281953979703ae71417f12e9dab38
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-02-08 00:49:43 +0600

    sendresponse

--- src/app/module/specialty/specialty.controller.ts
+++ src/app/module/specialty/specialty.controller.ts
@@ -1,15 +1,18 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+import {  Request,  Response } from "express";
 import { SpecialtyService } from "./specialty.service";
 import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
+
 
 const createSpecialty = catchAsync(async(req:Request, res:Response)=> {
     const result = await SpecialtyService.createSpecialty(req.body)
-    res.status(201).json({
+    sendResponse(res, {
+        httpStatusCode: 201,
         success: true,
         message: "Specialty created successfully",
         data: result
     })
-} )
+})
 
 
 
@@ -18,7 +21,8 @@ const createSpecialty = catchAsync(async(req:Request, res:Response)=> {
 
 const getAllSpecialties = catchAsync(async(req:Request, res:Response)=> {
     const result = await SpecialtyService.getAllSpecialties()
-    res.status(200).json({
+    sendResponse(res, {
+        httpStatusCode: 200,
         success: true,
         message: "Specialties fetched successfully",
         data: result
@@ -29,7 +33,8 @@ const getAllSpecialties = catchAsync(async(req:Request, res:Response)=> {
 const updateSpecialty = catchAsync(async(req:Request, res:Response)=> {
     const {id} = req.params;
     const result = await SpecialtyService.updateSpecialty(id as string, req.body)
-    res.status(200).json({
+    sendResponse(res, {
+        httpStatusCode: 200,
         success: true,
         message: "Specialty updated successfully",
         data: result
@@ -40,7 +45,8 @@ const updateSpecialty = catchAsync(async(req:Request, res:Response)=> {
 const deleteSpecialty = catchAsync(async(req:Request, res:Response)=> {
     const {id} = req.params;
     const result = await SpecialtyService.deleteSpecialty(id as string)
-    res.status(200).json({
+    sendResponse(res, {
+        httpStatusCode: 200,
         success: true,
         message: "Specialty deleted successfully",
         data: result
