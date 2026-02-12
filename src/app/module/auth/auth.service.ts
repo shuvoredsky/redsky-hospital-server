3582ae730a24e2a7286150b6c0c1b820e132297b
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-02-12 21:09:51 +0600

    set refresh token accesstoken

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { UserStatus } from "../../../generated/prisma/enums";
 import AppError from "../../errorHelpers/AppError";
 import { auth } from "../../lib/auth";
 import { prisma } from "../../lib/prisma";
+import { tokenUtils } from "../../utils/token";
 
 
 interface IRegisterPatientPayload {
@@ -91,7 +92,34 @@ const loginPatient = async(
         throw new AppError(status.NOT_FOUND, "Your account is deleted. Please contact support.");
     }
 
-    return data;
+    const accessToken = tokenUtils.getAccessToken({
+        userId: data.user.id,
+        role: data.user.role,
+        name: data.user.name,
+        email: data.user.email,
+        status: data.user.status,
+        isDeleted: data.user.isDeleted,
+        emailVerified: data.user.emailVerified,
+
+    })
+
+    const refreshToken = tokenUtils.getRefreshToken({
+        userId: data.user.id,
+        role: data.user.role,
+        name: data.user.name,
+        email: data.user.email,
+        status: data.user.status,
+        isDeleted: data.user.isDeleted,
+        emailVerified: data.user.emailVerified,
+
+    })
+
+    return {
+        ...data,
+        accessToken,
+        refreshToken
+    }
+
 }
 
 export const AuthService = {
